5a2babfc0236618f820aefb597cc983510831865
kassu11 <kasper.tamm@gmail.com>
2020-12-15 11:37:48 +0200

    koodin siistintää

--- js/code.js
+++ js/code.js
@@ -3,7 +3,6 @@ const currentHP = document.querySelector("#currenthp");
 const middleHole = document.querySelector("#middlehole");
 
 const piirakka = document.querySelector("div.pie");
-const piirakka2 = document.querySelector("div.pie2");
 const hptext = document.querySelector("#hptext");
 
 const poista = document.querySelector("#poista");
@@ -11,14 +10,6 @@ const lisaa = document.querySelector("#lisaa");
 
 hptext.textContent = currentHP.value + "hp / " + maxHP.value + "hp";
 piirakka.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
-piirakka2.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
-
-CSS.registerProperty({
-  name: "--value",
-  syntax: "<percentage>",
-  initialValue: "0%",
-  inherits: false
-});
 
 maxHP.addEventListener("input", vaihdaMaxHp);
 function vaihdaMaxHp() {
@@ -28,18 +19,15 @@ function vaihdaMaxHp() {
   hptext.textContent = currentHP.value + "hp / " + maxHP.value + "hp";
 
   piirakka.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
-  piirakka2.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
 }
 
 currentHP.addEventListener("input", vaihdaCurrentHp);
 function vaihdaCurrentHp() {
   piirakka.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
-  piirakka2.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
   hptext.textContent = currentHP.value + "hp / " + maxHP.value + "hp";
 }
 
 middleHole.addEventListener("input", vaihdaMiddleHole);
 function vaihdaMiddleHole() {
   piirakka.style.setProperty("--reika", middleHole.value);
-  piirakka2.style.setProperty("--reika", middleHole.value);
 }
