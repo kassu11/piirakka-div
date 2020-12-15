adc95c6569f0a74fc50aaee3b8796bf78adb5df4
kassu11 <kasper.tamm@gmail.com>
2020-12-15 11:34:37 +0200

    Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projektin idea
+
+- Kokeilin pystyinkö tekemään ilman canvas elementtiä piirakan
+- Ajattelin että voisi olla mukava hp baari tyyli, jos saa muokattua tarpeeksi
+- Toi systeemi ei kuitenkaan oo varmaan niin venyvä kun haluisin
--- js/code.js
+++ js/code.js
@@ -3,11 +3,22 @@ const currentHP = document.querySelector("#currenthp");
 const middleHole = document.querySelector("#middlehole");
 
 const piirakka = document.querySelector("div.pie");
+const piirakka2 = document.querySelector("div.pie2");
 const hptext = document.querySelector("#hptext");
 
+const poista = document.querySelector("#poista");
+const lisaa = document.querySelector("#lisaa");
 
 hptext.textContent = currentHP.value + "hp / " + maxHP.value + "hp";
-piirakka.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100);
+piirakka.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
+piirakka2.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
+
+CSS.registerProperty({
+  name: "--value",
+  syntax: "<percentage>",
+  initialValue: "0%",
+  inherits: false
+});
 
 maxHP.addEventListener("input", vaihdaMaxHp);
 function vaihdaMaxHp() {
@@ -16,16 +27,19 @@ function vaihdaMaxHp() {
   currentHP.value = Math.min(maxHP.value, currentHP.value);
   hptext.textContent = currentHP.value + "hp / " + maxHP.value + "hp";
 
-  piirakka.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100);
+  piirakka.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
+  piirakka2.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
 }
 
 currentHP.addEventListener("input", vaihdaCurrentHp);
 function vaihdaCurrentHp() {
-  piirakka.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100);
+  piirakka.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
+  piirakka2.style.setProperty("--value", 100 - (currentHP.value / maxHP.value) * 100 + "%");
   hptext.textContent = currentHP.value + "hp / " + maxHP.value + "hp";
 }
 
 middleHole.addEventListener("input", vaihdaMiddleHole);
 function vaihdaMiddleHole() {
   piirakka.style.setProperty("--reika", middleHole.value);
+  piirakka2.style.setProperty("--reika", middleHole.value);
 }
